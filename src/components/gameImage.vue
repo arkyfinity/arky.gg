b02499c385f003f83f0fdb9f246f036a948b2c91
Arkyfinity <hello@arky.dk>
2023-10-08 12:28:46 +0200

    Renamed for Vite-SSG, added missing imports, converted to composition API

--- src/components/gameImage.vue
+++ src/components/gameImage.vue
@@ -1,32 +1,32 @@
-<script lang="ts">
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import twitchapi from '@/helpers/twitchapi';
 
-export default {
-    props: ['gameTitle'],
-    data: () => ({
-        imageSource: ''
-    }),
+const props = defineProps<{
+    gameTitle: string;
+}>();
 
-    methods: {
-        fetchImage() {
-            twitchapi.get(`/games?name=${this.gameTitle}`)
-            .then((data: any) => {
-                const gameData = data.data.data;
-                const boxArt = gameData.find((item: any) => {
-                    return item.box_art_url;
-                });
-                boxArt ? this.imageSource = boxArt.box_art_url.replace('{width}x{height}', '144x192') : '';
-            })
-            .catch((error) => {
-                console.log(error);
+const imageSource = ref<string>('');
+
+const fetchImage = () => {
+    twitchapi.get(`/games?name=${props.gameTitle}`)
+        .then((data: any) => {
+            const gameData = data.data.data;
+            const boxArt = gameData.find((item: any) => {
+                return item.box_art_url;
             });
-        }
-    },
-
-    mounted() {
-        this.fetchImage();
-    },
+            if (boxArt) imageSource.value = boxArt.box_art_url.replace('{width}x{height}', '144x192');
+            else imageSource.value = '';
+        })
+        .catch((error) => {
+            // eslint-disable-next-line no-console
+            console.log(error);
+        });
 };
+
+onMounted(() => {
+    fetchImage();
+});
 </script>
 
 <template>
